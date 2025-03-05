d09ec5ea2b95a6f02d0b9449087069e6365378e4
nariwaa <lucasval3n2c@gmail.com>
2025-03-05 22:29:58 +0100

    added shell hook for zig

--- zig/shell.nix
+++ zig/shell.nix
@@ -7,5 +7,35 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
+    echo -e "\033[31mCreating .telescopeignore and .gitignore if not done already...\033[0m"
+
+    # Function to check if a line exists in a file
+    lineinfile() {
+        local file="$1"
+        local line="$2"
+        
+        if [ ! -f "$file" ]; then
+            touch "$file"
+        fi
+        
+        if ! grep -q -F "$line" "$file"; then
+            echo "$line" >> "$file"
+            echo -e "\033[31m    Added '$line' to $file\033[0m"
+        else
+            echo -e "\033[31m    '$line' already exists in $file, doing nothing."
+        fi
+    }
+
+    # telescope
+    lineinfile ".telescopeignore" "./.pythonlib/"
+
+    # gitignore
+    lineinfile ".gitignore" ""
+    lineinfile ".gitignore" "ig-cache/
+    lineinfile ".gitignore" "ig-out/
+    lineinfile ".gitignore" "uild/
+    lineinfile ".gitignore" "uild-*/
+    lineinfile ".gitignore" "ocgen_tmp/
+    echo -e "\033[33m\nDone!\033[0m"
   '';
 }
